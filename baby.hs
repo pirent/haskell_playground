5b279e172fbe130204f155748f6ba4ca13ffa2bd
pirent <pirent420@gmail.com>
2020-02-02 17:58:33 +0800

    Add let binding expression and case expression

--- baby.hs
+++ baby.hs
@@ -72,3 +72,17 @@ initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
 
 calcBmis xs = [bmi w h | (w, h) <- xs]
   where bmi weight height = weight / height ^ 2
+
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+calcBmisFat xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi >= 25.0]
+
+-- Let binding expression
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+  in sideArea + 2 * topArea
+
+-- Case expression
+describeList xs = "The list is " ++ case xs of [] -> "empty."
+                                               [x] -> "singleton list"
+                                               xs -> "a longer list"
