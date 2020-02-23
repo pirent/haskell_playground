cff579245e888d80ac8b0ffcc83cc597a0c40d85
pirent <pirent420@gmail.com>
2020-02-23 16:07:02 +0800

    Add function composition

--- high_order_function.hs
+++ high_order_function.hs
@@ -33,3 +33,48 @@ mapfr f xs = foldr (\x acc -> f x : acc) [] xs
 -- fold left with ++ function is much more expensive than :
 -- so we usually use right fold when building up new list from a list
 mapfl f xs = foldl (\acc x -> acc ++ [f x]) [] xs
+
+-- foldl1 works similar to fold without the starting value
+-- because it takes the first (last) element of the list as this value
+sumf1 :: (Num a) => [a] -> a
+sumf1 = foldl1 (+)
+
+-- a bunch of standard library reimplemented with fold
+maximumf' :: (Ord a) => [a] -> a
+maximumf' xs = foldr1 (\x acc -> if x > acc then x else acc) xs
+
+reversef :: [a] -> [a]
+reversef xs = foldl (\acc x -> x : acc) [] xs
+
+productf :: (Num a) => [a] -> a
+productf = foldr1 (*)
+
+filterf :: (a -> Bool) -> [a] -> [a]
+filterf f = foldr (\x acc -> if f x then x : acc else acc) []
+
+-- head is better implemented in pattern matching, but this just goes to show
+headf :: [a] -> a
+headf = foldl1 (\x _ -> x)
+
+lastf :: [a] -> a
+lastf = foldr1 (\_ x -> x)
+
+-- scanl and scanr are similar to foldl and foldr,
+-- except that they reports only the intermediate result of accumulator
+-- Example:
+-- > scanl (+) 0 [1,2,3,4]
+-- [0,1,3,6,10]
+-- > scanr (+) 0 [1,2,3,4]
+-- [10,9,7,4,0]
+
+sqrtSums :: Int
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
+
+-- function composition
+oddSquareSum = sum . takeWhile (<10000) . filter odd . map (^2) $ [1..]
+-- in order to avoid a long function composition chain
+-- use let bindings to give labels to intermediate results
+oddSquareSum' = 
+  let oddSquares = filter odd $ map (^2) [1..]
+      belowLimit = takeWhile (<10000) oddSquares
+  in sum belowLimit
